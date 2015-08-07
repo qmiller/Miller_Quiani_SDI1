c0b44ba57e6adbcd8454856f088cadbf95a3b125
Quiani Miller <qamiller@fullsail.edu>
2015-08-07 00:21:55 -0400

    Screencast follow along
    Screencast follow along

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,21 @@
+// JavaScript
+
+var yearBorn; declaration//
+yearBorn= 1984;//definition
+//storage container = value
+
+var age = 24; //declaring and defining on the same line
+
+
+/*
+console.log(yearBorn)
+console.log("Hello World!")
+ */
+
+alert(yearBorn)
+
+var firstName = "Quiani"
+
+var isStudent = false; //Boolean Variable
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor.';
