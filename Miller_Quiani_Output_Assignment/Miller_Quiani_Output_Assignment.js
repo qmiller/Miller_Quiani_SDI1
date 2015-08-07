dd14b4aa2ed0ab7d615326c89b59c5cfcd572eb1
Quiani Miller <qamiller@fullsail.edu>
2015-08-07 07:30:35 -0400

    Added variables and index file
    Added variables and index file.

--- Miller_Quiani_Output_Assignment/Miller_Quiani_Output_Assignment.js
+++ Miller_Quiani_Output_Assignment/Miller_Quiani_Output_Assignment.js
@@ -9,4 +9,9 @@ jerseyNumber = 10 //defining variable
 
 var isAllstar = true ;
 
+//alert(jerseyNumber); //testing jerseyNumber
+
+var phrase = "I play softball. I wear number";
+alert[phrase(jerseyNumber)];
+
 
