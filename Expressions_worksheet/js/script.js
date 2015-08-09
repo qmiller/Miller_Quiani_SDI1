5fbe1d674379a5971fc603300870a695cf2ca250
Quiani Miller <qamiller@fullsail.edu>
2015-08-09 00:20:32 -0400

    Average shopping bill
    Expressions worksheet problem number 4 average shopping bill.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -10,20 +10,34 @@
 //console.log(result);
 
 /*
+//Dog Years
 var sparkyAge = 5; //age of the dog in human years
 var actualAge = sparkyAge * 7; //calculates the age of the dog in dog years
 var result = "Sparky is " + sparkyAge + " human years old which is " + actualAge + " in dog years."; // gives the result of the calculation and forms statement
 console.log(result); //prints the statement
 */
 
-
+/*
+//Slice of Pie part 1
 var slicesPizza = 8; //number of slices per pizza
 var partyGoers = 10; //number of people attending the party
 var pizzasOrdered = 4; //number of pizzas ordered
 var numSlices = slicesPizza * pizzasOrdered / partyGoers; // calculates the number of slices each persons will eat
 //var result = "Each person ate " + numSlices + " slices of pizza at the party."; //prints the result in a statement.
 //console.log(result); //prints the result in the console.
+
+//Slice of Pie part 2
 var sparkySlices = slicesPizza * pizzasOrdered % partyGoers; //uses modulo operator to get the number of slices of pizza that Sparky gets
 var result = "Sparky got " + sparkySlices + " slices of pizza."; //prints the result in a statement
-console.log(result); //prints the result in the console. 
+console.log(result); //prints the result in the console.
+*/
+
+/*
+//Average shopping bill
+var groceryBill = [129.45, 175.99, 289.12, 193.11, 188]; //defines an array with the grocery bills
+var groceryTotal = groceryBill[0] + groceryBill[1] + groceryBill[2] + groceryBill[3] + groceryBill[4]; //adds the total of the grocery bills
+var groceryAverage = groceryTotal/5; //gets the average of the grocery bills
+var result = "You have spent a total of " + groceryTotal + " on groceries over 5 weeks. \nThat is an average of " + groceryAverage + " per week."; //prints the reult into a statement
+console.log(result); //prints the result into the console.
+*/
 
