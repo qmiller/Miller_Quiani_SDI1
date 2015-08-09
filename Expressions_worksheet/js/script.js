963e1e1e4ec0df97915be438285174558c9686c1
Quiani Miller <qamiller@fullsail.edu>
2015-08-08 23:33:36 -0400

    Slices of pie part 1
    Expressions worksheet problem number 2 slices of pie part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,10 +9,17 @@
 //var result = "The area of the rectangle is " + areaRectangle + " square feet."
 //console.log(result);
 
-
+/*
 var sparkyAge = 5; //age of the dog in human years
 var actualAge = sparkyAge * 7; //calculates the age of the dog in dog years
 var result = "Sparky is " + sparkyAge + " human years old which is " + actualAge + " in dog years."; // gives the result of the calculation and forms statement
 console.log(result); //prints the statement
+*/
 
+var slicesPizza = 8; //number of slices per pizza
+var partyGoers = 20; //number of people attending the party
+var pizzasOrdered = 10; //number of pizzas ordered
+var numSlices = slicesPizza * pizzasOrdered / partyGoers; // calculates the number of slices each persons will eat
+var result = "Each person ate " + numSlices + " slices of pizza at the party."; //prints the result in a statement.
+console.log(result); //prints the result in the console.
 
