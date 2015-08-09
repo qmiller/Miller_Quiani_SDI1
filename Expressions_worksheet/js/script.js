8bc016d5bafd75e7dffc59336ce80aa5492310cf
Quiani Miller <qamiller@fullsail.edu>
2015-08-08 23:28:21 -0400

    Dog Years
    Expressions worksheet problem number one dog years.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Quiani Miller on 8/8/15.
+ */
+//Expressions Worksheet
+
+//var widthRectangle = 4; //declare and define the width of the rectangle
+//var heightRectangle = 4; //declare and define the height of the rectangle
+//var areaRectangle =  widthRectangle * heightRectangle; // calculates the area of the rectangle
+//var result = "The area of the rectangle is " + areaRectangle + " square feet."
+//console.log(result);
+
+
+var sparkyAge = 5; //age of the dog in human years
+var actualAge = sparkyAge * 7; //calculates the age of the dog in dog years
+var result = "Sparky is " + sparkyAge + " human years old which is " + actualAge + " in dog years."; // gives the result of the calculation and forms statement
+console.log(result); //prints the statement
+
+
