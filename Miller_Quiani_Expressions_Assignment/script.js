0ab937678ebbbb59aeef3a5e2ac1640651bd8fb0
Quiani Miller <qamiller@fullsail.edu>
2015-08-11 01:02:37 -0400

    If Statement
    Created and added an “if” statement to decide which way to add up the
    utilities.

--- Miller_Quiani_Expressions_Assignment/script.js
+++ Miller_Quiani_Expressions_Assignment/script.js
@@ -1,23 +1,36 @@
-/**
- * Created by Quiani Miller on 8/10/15.
+/*
+Created by Quiani Miller
+August 10, 2015
+SDI 1508
+Expressions Assignment
  */
 
 
 var rent = 750; //variable for the rent which does not change
 var cox = 175; //variable for the cox service which does not change
 var name = prompt("Please enter your first name:"); //prompt to get the user name so the output can be personalized
-var numRoommates = prompt("How many roommates do you have?");//prompt for the number of roommates so that the proper math can be calculated.
+var numRoommates = prompt("How many roommates do you have including yourself?");//prompt for the number of roommates so that the proper math can be calculated.
 var electric = prompt("How much is your electric bill?");//prompt for user input for cost of electric bill
-var water = prompt ("How much is your water bill"); //prompt for user input for cost of water bill
-var trash = prompt("How much is your sewer and trash bill?"); //prompt for user input for cost of sewer and trash
 var gas = prompt("How much is your gas bill?"); //prompt for user input for cost of gas bill
-var utilities =[Number(electric), Number(water), Number(gas), Number(trash)]; //array designed to hold all utilities.
+
+var month = prompt("Does this month include water and trash? Yes or No. \nType 1 for yes. \nType 2 for no.");
+var utilities =[Number(electric), Number(gas), Number(water), Number(trash)]; //array designed to hold all utilities.
+
+if(month == 1) { //if statement used to decide which bills to include
+
+    //alert("yes");//used alert to test correct function of if statement
+    var water = prompt ("How much is your water bill"); //prompt for user input for cost of water bill
+    var trash = prompt("How much is your trash bill?"); //prompt for user input for cost of sewer and trash
+    var totalUtilities = utilities[0] + utilities[1] + utilities[2] + utilities[3] + rent + cox; //math done to get the total of all necessary utilities
+
+}//closes if statement
 
 
-var totalUtilities = utilities[0] + utilities[1] + utilities[2] + utilities[3] + rent + cox; //math done to get the total of the array
+
+    //alert("no");//used alert to test the correct function of if statement
+    var totalUtilities = utilities[0] + utilities[1] + rent + cox; //math done to get the total of utilities without water and trash
+
 var total = totalUtilities / numRoommates; //math done to get total that will need to be paid by each roommate.
-
-
 
 
 var result = name + " your monthly bill total is $" + totalUtilities + ". You have " + numRoommates + " roommates. Your total due this month is $" + total + "."; //variable created for printing out a statement.
