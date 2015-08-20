561f653cb02ceceec8719c8dcfb0fae03e07871b
Quiani Miller <qamiller@fullsail.edu>
2015-08-20 10:45:54 -0400

    Conditional Assignment
    Set up folder tree and all variables.

---
+++ Miller_Quiani_Conditionals_Assignment/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Quiani Miller
+ * August 20, 2015
+ * SDI 1508
+ * Conditionals Assignment
+ */
+
+//Body Fat Percentage Calculator. This will tell the user how much body fat they have and if they need to loose any weight.
+    //All the variable to be used in the calculator.
+var name;
+var gender;
+var waist;
+var wrist;
+var hip;
+var forearm;
+var factor1;
+var factor2;
+var factor3;
+var factor4;
+var factor5;
+var leanBody;
+var fatWeight;
+var fatPercent;
