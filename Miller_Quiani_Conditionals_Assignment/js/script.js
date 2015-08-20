b5ed3532a27be75af6a47d616448863768509707
Quiani Miller <qamiller@fullsail.edu>
2015-08-20 11:34:18 -0400

    progress in code
    progress in code

--- Miller_Quiani_Conditionals_Assignment/js/script.js
+++ Miller_Quiani_Conditionals_Assignment/js/script.js
@@ -21,3 +21,57 @@ var factor5;
 var leanBody;
 var fatWeight;
 var fatPercent;
+
+
+name = prompt("Please enter you name: ");
+if (name=="") {
+
+    console.log("I asked you to enter your name please?!");
+    name = prompt("Please enter you name: ");
+
+}
+
+    gender = prompt("Are you male or female. \nPlease enter 1 for male or 2 for female.");
+    if (gender == ""){
+        console.log("Please select you gender you are messing up my calculator!");
+        gender = prompt("Are you male or female. \nPlease enter 1 for male or 2 for female.");
+
+    }
+        alert("Please enter only the a number going forward! Thank you.");
+
+    if (gender == 1) {
+        weight = prompt("Please enter your weight.");
+        if (weight == ""){
+            console.log("You are hard-headed sir and trying to mess up my code. I don't appreciate that.");
+            weight = prompt("Please enter your weight.");
+        }
+        waist = prompt("Please enter your waist measurement. \nMeasure your waist at the naval.");
+
+        factor1 = (Number(weight) * 1.082) + 94.42;
+        factor2 = Number(waist) * 4.15;
+        leanBody = factor1 - factor2;
+        fatWeight = Number(weight) - leanBody;
+        fatPercent = (fatWeight * 100) / Number(weight);
+        alert("male");
+
+    }else{
+        weight = prompt("Please enter your weight.");
+        if (weight == ""){
+            console.log("You are hard-headed ma'am and trying to mess up my code. I don't appreciate that.");
+            weight = prompt("Please enter your weight.");
+        }
+        waist = prompt("Please enter your waist measurement. \nMeasure your waist at the naval.");
+        wrist = prompt("Please enter your wrist measurement.");
+        hip = prompt("Please enter your hip measurement");
+        forearm = prompt("Please enter your forearm measurement");
+
+        factor1 = (Number(weight) * 0.732) + 8.987;
+        factor2 = Number(wrist) / 3.140;
+        factor3 = Number(waist) * 0.157;
+        factor4 = Number(hip) * 0.249;
+        factor5 = Number(forearm) * 0.434;
+        leanBody = factor1 + factor2 - factor3 - factor4 + factor5;
+        fatWeight = Number(weight) - leanBody;
+        fatPercent = (fatWeight * 100) / Number(weight);
+        alert("female");
+    }
