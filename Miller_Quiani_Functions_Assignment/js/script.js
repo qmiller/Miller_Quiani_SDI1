de7b562ac66b7b020d3f017e26d8c36eea0d86e7
Quiani Miller <qamiller@fullsail.edu>
2015-08-27 12:38:15 -0400

    switch attempt
    Trying to use a switch to ensure no duplicating numbers

--- Miller_Quiani_Functions_Assignment/js/script.js
+++ Miller_Quiani_Functions_Assignment/js/script.js
@@ -37,32 +37,93 @@ function nameValidation(firstName) {
 }
 function powerBall(min, max, num){
 
-    alert("Powerball numbers");
+    alert("Good luck here are your powerball numbers");
     var randomNum = [];
 
    for (var i = 0; i < num; i++) {
 
        randomNum[i] = Math.random() * (max - min) + min;
        randomNum[i] = Math.round(randomNum [i]);
-       console.log(randomNum);
+
+
    }
+    console.log(randomNum);
     return randomNum;
 }
 
-function lotto(){
+function lotto(min, max, num){
 
-    alert("Lottery numbers!!!");
+    alert("Good luck, Here are your lottery numbers!!!");
     var randomNum = [];
 
     for (var i = 0; i < num; i++) {
 
         randomNum[i] = Math.random() * (max - min) + min;
         randomNum[i] = Math.round(randomNum [i]);
-        console.log(randomNum);
+
     }
+    console.log(randomNum);
     return randomNum;
 }
 
+function powerBall2(min, max, num){
+    var randomNum;
+    var powerball;
+    for(var i = 0 ; i < num; ++i) {
+        var ball1 = 0;
+        var ball2 = 0;
+        var ball3 = 0;
+        var ball4 = 0;
+        var ball5 = 0;
+        var ballCount = 0;
+
+        while(ballCount<num){
+
+            randomNum = Math.random() * (max - min) + min;
+            randomNum = Math.round((randomNum));
+
+
+            switch(powerball){
+
+                case 0:
+                    ball1 = randomNum;
+                    num++;
+                    break;
+
+                case  1:
+                    if(randomNum != ball1) {
+                        ball2 = randomNum;
+                        num++;
+                    }
+                    break;
+
+                case 2:
+                    if(randomNum != ball1 && randomNum != ball2) {
+                        ball3 = randomNum;
+                        num++;
+                    }
+                    break;
+
+                case 3:
+                    if(randomNum != ball1 && randomNum != ball2 && randomNum != ball3){
+                        ball4 = randomNum;
+                        num++;
+                    }
+                    break;
+                case 4:
+                    if(randomNum != ball1 && randomNum != ball2 && randomNum != ball3 && randomNum != ball4){
+                        ball5 = randomNum;
+                        num++;
+                    }
+                    break;
+
+
+            }
+        }
+
+        console.log(ball1, ball2, ball3, ball4, ball5);
+    }
+}
 
 
 //math.random() * (max - min ) + min
@@ -76,4 +137,4 @@ function lotto(){
 //powerBall();
 //lotto();
 
-(gameType == true) ? lotto() : powerBall();
+(gameType == true) ? lotto(1, 53, 6) : powerBall2(1, 59, 5);
